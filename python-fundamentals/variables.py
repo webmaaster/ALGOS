a88511f389a5da2c1289a79c061511a0d0a1c9d3
Software Engineer <softwareengineer@LambdaSr233sMBP.home>
2020-06-02 15:05:15 -0400

    Complete hell_world.py assignment

---
+++ python-fundamentals/variables.py
@@ -0,0 +1,33 @@
+name = "Aski"
+print(name)
+print("My daughter's name is", name)
+print("My elder daughter's name is " + name)
+
+
+
+firstName = "Babul"
+lastName = "Nokrek"
+age = 45
+print(f"My name is {firstName} {lastName} and I am {age} years old.")
+
+
+
+firstName = "Monica"
+lastName = "Chisim"
+age = 44
+
+print("My wife's name is {} {} and she {} years old.".format(firstName, lastName, age))
+
+
+
+favFoodOne = "Chicken"
+favFoodTwo = "Vegetables"
+
+print(f"My favorite foods are {favFoodOne} and {favFoodTwo}.")
+
+
+
+favFoodOne = "Parata"
+favFoodTwo = "Veg"
+
+print(f"My favorite foods are {} and {}.".format(favFoodOne, favFoodTwo))
